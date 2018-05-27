7d4ff8336a64b0d602f8b2357a7ee2382c5ee4fb
rulinux2011 <31537453+rulinux2011@users.noreply.github.com>
2018-05-28 03:39:18 +0800

    Add files via upload

--- oj8.cpp
+++ oj8.cpp
@@ -19,44 +19,25 @@
 #include<iostream>
 #include<cstring>
 using namespace std;
-int array_size(int n,char array[]){
-            int k=0;
-            for(int i=0;i<n;++i){
-                      if(array[i]==','){
-                        k++;
-                      }
-            }
-            return k;
-}
-void convert(int n,char array[],int array2[]){
-            int j=0;
-            for(int i=0;i<n;++i){
-                        if(array[i]!=','){
-                                    array2[j++]=array[i]-'0';
+
+int  convert_1(int n,char array[])
+{
+            int res=0,l=1;
+            for(int i=n-1;i>=0;--i)
+            {
+                        if(array[i]!=',')
+                        {
+                               res+=(array[i]-'0')*l;
+                               l*=10;
                         }
             }
+            return res;
 }
-void change(int n,int array[]){
-           for(int i=0;i<n/2;++i){
-                     int temp=array[n-1-i];
-                    array[n-1-i]=array[i];
-                    array[i]=temp;
-           }
-}
-int sum(int n,int array[]){
-           int  sum=0;
-           int  l=1;
-            for(int i=0;i<n;++i){
-                        sum+=array[i]*l;
-                        l*=10;
-            }
-            return sum;
-}
+
+
 int main(){
             char a[12];
-            int temp1[12];
             char b[12];
-            int temp2[12];
             while(cin>>a>>b){
             int n1=strlen(a);
             int n2=strlen(b);
@@ -66,33 +47,25 @@ int main(){
                     for(int i=0;i<n1-1;++i){
                         a[i]=a[i+1];
                     }
-                       N=n1-1-array_size(n1-1,a);
-                       convert(n1-1,a,temp1);
+                       N=n1-1;
                 }else{
                        flag=1;
-                       N=n1-array_size(n1,a);
-                       convert(n1,a,temp1);
+                       N=n1;
             }
              if(*b=='-'){
                     for(int i=0;i<n2-1;++i){
                         b[i]=b[i+1];
                     }
-                       M=n2-1-array_size(n2-1,b);
-                       convert(n2-1,b,temp2);
+                       M=n2-1;
                 }else{
                        flag2=1;
-                       M=n2-array_size(n2,b);
-                       convert(n2,b,temp2);
+                       M=n2;
             }
 
-
-             change(N,temp1);
              int A;
-             A=flag*sum(N,temp1);
-
-             change(M,temp2);
+             A=flag*convert_1(N,a);
              int B;
-             B=flag2*sum(M,temp2);
+             B=flag2*convert_1(M,b);
 
              cout<<A+B<<endl;
 
